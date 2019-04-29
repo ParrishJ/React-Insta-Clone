64adef163ca20e6ed1f34d53ecbc1593b8a88148
ParrishJ <jarparrish@gmail.com>
2019-04-29 13:54:54 -0700

    progress on app

--- instagram-app/src/Components/SearchBar/SearchBar.js
+++ instagram-app/src/Components/SearchBar/SearchBar.js
@@ -1,1 +1,23 @@
 // Search Bar js file
+
+import React from 'react';
+import './SearchBar.css';
+
+const SearchBar = props => {
+    return (
+        <div className="topNav">
+            <div className="topLogos">
+            </div>
+            <div className="search">
+                <input type="text" placeholder="search"></input>
+            </div>
+            <div className ="profileNav">
+                <a href="#"></a>
+                <a href="#"></a>
+                <a href="#"></a>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
